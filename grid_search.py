08657db937d083a1d80890e4491977afc6c89e1e
Taresh Sandhu <taresh.sandhu@gmail.com>
2020-02-09 14:56:09 -0800

    final main

--- grid_search.py
+++ grid_search.py
@@ -16,22 +16,27 @@ from sklearn.model_selection import train_test_split, learning_curve
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, classification_report
 import seaborn as sns
-# plt.switch_backend('agg')
+plt.switch_backend('agg')
 import time
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import RandomizedSearchCV
 from collections import OrderedDict
 from textwrap import wrap
+import datetime
 
+seed = 903387974
+
+LOG_FILENAME = datetime.datetime.now().strftime('logfile_%H_%M_%d_%m_%Y.log')
 
 logger: logging.getLogger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 if not logger.hasHandlers():
-    sh = logging.StreamHandler()
+    fh = logging.FileHandler(LOG_FILENAME)
+    # sh = logging.StreamHandler()
     fmt = logging.Formatter(fmt="[%(asctime)s] [%(process)d] %(name)-12s %(levelname)-8s %(message)s")
-    sh.setFormatter(fmt)
-    logger.addHandler(sh)
+    fh.setFormatter(fmt)
+    logger.addHandler(fh)
     logger.propagate = False
 
 verbose = False
@@ -54,43 +59,13 @@ def report(results, n_top=1):
             logger.info("Parameters: {0}".format(results['params'][candidate]))
             logger.info("")
 
-
-def plot_grid_search(cv_results, grid_param_1, grid_param_2, name_param_1, name_param_2):
-    # Get Test Scores Mean and std for each grid search
-    score = pd.DataFrame(cv_results).sort_values(by=f"param_" + name_param_2, ascending=False)
-
-    score2 = pd.DataFrame(cv_results).sort_values(by="mean_test_score", ascending=False)
-
-    scores_mean = score['mean_test_score'].to_numpy()
-    scores_mean = np.array(scores_mean).reshape(len(grid_param_2), len(grid_param_1))
-
-    # Plot Grid search scores
-    _, ax = plt.subplots(1,1)
-
-    # Param1 is the X-axis, Param 2 is represented as a different curve (color line)
-    for idx, val in enumerate(grid_param_2):
-        ax.plot(grid_param_1, scores_mean[idx,:], '-o', label=name_param_2 + ': ' + str(val))
-
-    ax.set_title("Grid Search Scores", fontsize=20, fontweight='bold')
-    ax.set_xlabel(name_param_1, fontsize=16)
-    ax.set_ylabel('CV Average Score', fontsize=16)
-    ax.legend(loc="best", fontsize=15)
-    ax.grid('on')
-    ax.legend(bbox_to_anchor=(1.1, 1.05))
-    plt.show()
-    # plt.savefig('./grid_search/' + name + '_' + str(timestr) + '.png')
-
-
 def main():
-    seed = 903387974
+
     if verbose:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
 
-    if seed is None:
-        seed = np.random.randint(0, (2 ** 31) - 1)
-        logger.info("Using seed {}".format(seed))
     df_credit = pd.read_csv(data_directory / credit_card_file)
 
     # code copied from https://www.kaggle.com/kernels/scriptcontent/2094910/download
@@ -107,27 +82,25 @@ def main():
     x_scaler = StandardScaler()
     df_credit[features_credit] = x_scaler.fit_transform(df_credit[features_credit])
 
-    # Split 20% of data set for testing
-    x_train_credit, x_test_credit, y_train_credit, y_test_credit = train_test_split(
-        df_credit[features_credit], df_credit['def_pay'], test_size=0.2, random_state=seed)
-
     # load pen digits
-
     df_pen = pd.read_csv(data_directory / pen_digits_file)
     features_pen = ['input1', 'input2', 'input3', 'input4', 'input5', 'input6', 'input7', 'input8', 'input9',
                     'input10', 'input11', 'input12', 'input13', 'input14', 'input15', 'input16']
 
     df_pen[features_pen] = x_scaler.fit_transform(df_pen[features_pen])
 
+    train_sizes_pen = [100, 500, 1000, 1500, 3000, 4000, 5000, 6000, 7000, 8793]
+    run_grid_search(pen_digits_file, df_pen, features_pen, 'class', train_sizes_pen)
+
+    train_sizes_credit = [100, 500, 2000, 5000, 8000, 10000, 12000, 15000, 18000, 20000, 24000]
+    run_grid_search(credit_card_file, df_credit, features_credit, 'def_pay', train_sizes_credit)
+
+
+def run_grid_search(filename, df, features, pred, train_sizes):
+
     # Split 20% of data set for testing
-    x_train_pen, x_test_pen, y_train_pen, y_test_pen = train_test_split(
-        df_pen[features_pen], df_pen['class'], test_size=0.2, random_state=seed)
-
-    run_grid_search(pen_digits_file, x_train_pen, y_train_pen, x_test_pen, y_test_pen, seed)
-    run_grid_search(credit_card_file, x_train_credit, y_train_credit, x_test_credit, y_test_credit, seed)
-
-
-def run_grid_search(filename, x_train, y_train, x_test, y_test, seed):
+    x_train, x_test, y_train, y_test = train_test_split(
+        df[features], df[pred], test_size=0.2, random_state=seed)
 
     models = [
         (DecisionTreeClassifier(),
@@ -138,9 +111,11 @@ def run_grid_search(filename, x_train, y_train, x_test, y_test, seed):
          OrderedDict([('n_estimators', np.arange(10, 100, 10)), ('base_estimator__splitter', ["best", "random"])])),
         (KNeighborsClassifier(),
          'KNN',
-         OrderedDict([('n_neighbors', np.arange(1, 10)), ('weights', ["uniform","distance"])])),
+         OrderedDict([('n_neighbors', np.arange(1, 10)), ('weights', ["uniform", "distance"])])),
         (MLPClassifier(max_iter=1000),
-         'NN', OrderedDict([('hidden_layer_sizes', [x for x in itertools.product((10, 20, 30, 40, 50, 100), repeat=3)]),
+         'NN', OrderedDict([('hidden_layer_sizes',
+                             [(10, 10, 10), (20, 20, 50), (20, 20, 100), (20, 100, 50), (30, 10, 50), (30, 30, 100),
+                              (30, 40, 10), (100, 100, 50), (100, 100, 100)]),
                             ('activation', ['tanh', 'relu'])])),
         (SVC(kernel='rbf'),
          'SVM rbf',
@@ -153,7 +128,6 @@ def run_grid_search(filename, x_train, y_train, x_test, y_test, seed):
               ]
 
     for model, name, ordered_dict in models:
-        # timestr = time.strftime("%Y%m%d-%H%M`1`S")
         logger.info(f"Calling GridSearch On {name} {filename}")
         items = list(ordered_dict.items())
         param_1_name = items[0][0]
@@ -167,12 +141,49 @@ def run_grid_search(filename, x_train, y_train, x_test, y_test, seed):
                                   n_iter=len(param_1) * len(param_2), cv=5, random_state=seed, n_jobs=-1,
                                   scoring='neg_mean_squared_error')
         grid.fit(x_train, y_train)
+        plt.clf()
         plot_grid(name + ' ' + filename, grid, param_1_name, param_2_name)
         best = grid.best_estimator_
         logger.info(best)
-        y_true, y_pred_test = y_test, grid.predict(x_test)
+
+        y_true, y_pred_test = y_test, grid.best_estimator_.predict(x_test)
         report(grid.cv_results_)
-        logger.info(classification_report(y_true, y_pred_test))
+        test_report = classification_report(y_true, y_pred_test, output_dict=True)
+        df_test_report = pd.DataFrame(test_report).transpose()
+        # save report to disk
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        reports_folder = Path("reports/")
+
+        test_report_name = f"{name}_{filename}_Test_" + str(timestr) + '.csv'
+        reports_file_path = reports_folder / test_report_name
+        df_test_report.to_csv(reports_file_path)
+
+        #plot learing curves
+        plt.clf()
+        learning_curves(name + ' ' + filename, model, df, features, pred, train_sizes, 5)
+
+
+def learning_curves(name, estimator, data, features, target, train_sizes, cv):
+
+    train_sizes, train_scores, validation_scores = learning_curve(estimator, data[features], data[target],
+                                                                 train_sizes=train_sizes, cv=cv,
+                                                                 scoring ='neg_mean_squared_error')
+    train_scores_mean = -train_scores.mean(axis=1)
+    validation_scores_mean = -validation_scores.mean(axis=1)
+    plt.plot(train_sizes, train_scores_mean, label='Training error')
+    plt.plot(train_sizes, validation_scores_mean, label='Validation error')
+    plt.ylabel('MSE', fontsize=14)
+    plt.xlabel('Training set size', fontsize=14)
+    # title = 'Learning curves for a ' + str(estimator).split('(')[0] + ' model'
+    title = f"Best Fit Learning Curve %s" % "\n".join(wrap(f"{name}", width=100))
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    plt.subplots_adjust(top=0.95)
+    plt.title(title)
+    plt.margins(0)
+    plt.legend()
+    # plt.show()
+    plt.savefig('./grid_search_learning_curves/' + name + '_' + str(timestr) + '.png')
+    plt.close()
 
 
 def plot_grid(name, grid, param1, param2):
@@ -183,15 +194,10 @@ def plot_grid(name, grid, param1, param2):
         .reset_index()
     df_score = df_score.melt(f'param_' + param1, var_name=param2, value_name='mean_test_score')
     g = sns.catplot(x=f"param_" + param1, y="mean_test_score", hue=param2, data=df_score)
-    # leg = g._legend
-    # leg.set_bbox_to_anchor([1, 1])  # coordinates of lower left of bounding box
-    # leg._loc = 2  # if required you can set the loc
-    # plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
     title = f"RandomizedSearchCV for a %s" % "\n".join(wrap(f"{name}", width=150))
     g.fig.suptitle(title)
     timestr = time.strftime("%Y%m%d-%H%M%S")
     plt.savefig('./grid_search/' + name + '_' + str(timestr) + '.png')
-
 
 if __name__ == "__main__":
     main()
